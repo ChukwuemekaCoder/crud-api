b9876f3e36bb2c19d33961a5d0248bd54b72c3e9
Chukwuemeka Obinna <emekaobinna1207@gmail.com>
2026-07-22 15:31:14 -0500

    root and health endpoints

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
-
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from JavaScript!');
+  res.send('Hello World!');
+  res.json({
+    name: 'Task API',
+    version: '1.0',
+    endpoints: ['/tasks']
+  });
+});
+
+app.get('/health', (req, res) => {
+  res.json({
+    status: 'ok'
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
